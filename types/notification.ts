15594a25978a01fad71f339bffb9744241a5aeae
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 22:13:59 +0100

    Update notification.ts

--- types/notification.ts
+++ types/notification.ts
@@ -1,1 +1,9 @@
-
+export type Notification = {
+  id: string;
+  recruiterId: string;
+  title: string;
+  message: string;
+  type: "player_update" | "relation" | "payment" | "system";
+  read: boolean;
+  createdAt: string;
+};
