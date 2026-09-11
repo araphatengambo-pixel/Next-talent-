ff0d3a193a7437ec030ee797220ac871ac3e56dd
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 17:25:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Next Talent",
+  description:
+    "Plateforme de détection et de mise en relation des talents du football.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
