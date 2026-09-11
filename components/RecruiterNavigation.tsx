6e51d9ee5e94892ee18ca88fa8803431cd95bc2d
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 18:03:27 +0100

    Create RecruiterNavigation.tsx

---
+++ components/RecruiterNavigation.tsx
@@ -0,0 +1,12 @@
+export default function RecruiterNavigation() {
+  return (
+    <nav>
+      <ul>
+        <li>🏠 Accueil</li>
+        <li>⭐ Mes favoris</li>
+        <li>🔔 Notifications</li>
+        <li>👤 Profil</li>
+      </ul>
+    </nav>
+  );
+}
