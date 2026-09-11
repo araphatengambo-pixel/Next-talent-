7e1ee5c7e6c0b5f548fbb2420c68f5b07d5e10c2
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 22:00:07 +0100

    Update recruiter.ts

--- types/recruiter.ts
+++ types/recruiter.ts
@@ -1,1 +1,10 @@
-
+export type Recruiter = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  clubName: string;
+  country: string;
+  status: "pending" | "verified" | "rejected";
+  createdAt: string;
+};
