021774f0c3c622fe7c02dfec7f2be954d5a9e60d
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 17:44:10 +0100

    Create SearchBar.tsx

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Rechercher un joueur..."
+        aria-label="Rechercher un joueur"
+      />
+    </div>
+  );
+}
