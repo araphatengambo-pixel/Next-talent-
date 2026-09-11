71e3f60aac3dd17630c5bc6dc0b7d35f7ee9911f
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 18:20:12 +0100

    Update player.ts

--- types/player.ts
+++ types/player.ts
@@ -1,1 +1,15 @@
-
+export type Player = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  country: string;
+  position: string;
+  height: number;
+  weight: number;
+  speed: number;
+  strongFoot: "droit" | "gauche" | "deux";
+  videoUrl?: string;
+  photoUrl?: string;
+  status: "draft" | "published" | "disabled";
+};
