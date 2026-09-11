060ac7fe4878c9f2c427af7fbb313e68cf52c8dd
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 17:12:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main>
+      <h1>Next Talent</h1>
+      <p>Le talent n’attend pas d’être découvert. Il mérite d’être vu.</p>
+    </main>
+  );
+}
