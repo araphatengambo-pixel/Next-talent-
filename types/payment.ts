e5da25d290c3a6a8258926840352a4620b9a9936
araphatengambo-pixel <araphatengambo@gmail.com>
2026-09-11 22:14:41 +0100

    Update payment.ts

--- types/payment.ts
+++ types/payment.ts
@@ -1,1 +1,10 @@
-
+export type Payment = {
+  id: string;
+  recruiterId: string;
+  playerId?: string;
+  amount: number;
+  currency: string;
+  type: "relation" | "subscription";
+  status: "pending" | "paid" | "failed" | "refunded";
+  createdAt: string;
+};
